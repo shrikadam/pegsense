82ec08f5e7cbdc2123de680d31f5b4faf66657dd
shrikadam <shriniwaskadam25@gmail.com>
2025-05-25 12:10:41 +0530

    Added camera to EOAT

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import gymnasium
 import ur10e_mujoco_env
 
 # Create the environment with rendering in human mode
-env = gymnasium.make('ur10e_mujoco_env/UR10eMjEnv-v0', render_mode='human')
+env = gymnasium.make('ur_env/UR10eMjEnv-v0', render_mode='human')
 
 # Reset the environment with a specific seed for reproducibility
 observation, info = env.reset(seed=42)
